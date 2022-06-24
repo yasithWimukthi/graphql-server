07c3aff8d6e2e7a35f453840f99a45dc033ce45b
yasith <it19966922@my.sliit.lk>
2022-06-24 22:49:46 +0530

    create server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
 
 const app = express();
+
+app.use('/graphql',graphqlHTTP({
+
+}))
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
